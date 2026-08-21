f9fa60d3f82464869cab82359be1f3f989a8836d
Gabriel Saraiva <gabrielsaraiva1502@gmail.com>
2026-08-21 01:44:43 -0300

    feat: setup Angular project with routing and sidebar component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home').then(m => m.Home)
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./features/profile/profile').then(m => m.Profile)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard').then(m => m.Dashboard)
+  },
+  {
+    path: 'store',
+    loadComponent: () =>
+      import('./features/store/store').then(m => m.Store)
+  },
+  {
+    path: 'settings',
+    loadComponent: () =>
+      import('./features/settings/settings').then(m => m.Settings)
+  }
+];
