72ff4efb8664c68309dc625f2b1fde3d20e4944f
Gabriel Saraiva <gabrielsaraiva1502@gmail.com>
2026-08-23 19:07:27 -0300

    feat: improve sidebar styling and add logo as asset

--- client/src/app/shared/components/sidebar/sidebar.ts
+++ client/src/app/shared/components/sidebar/sidebar.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Sidebar {}
